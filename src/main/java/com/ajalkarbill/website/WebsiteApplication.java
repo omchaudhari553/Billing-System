3a60db02264e51ee19f9a432e6788eef13e0417c
omchaudhari553 <omchaudhari553@gmail.con>
2026-09-11 16:27:57 +0530

    Adding new changes

--- src/main/java/com/ajalkarbill/website/WebsiteApplication.java
+++ src/main/java/com/ajalkarbill/website/WebsiteApplication.java
@@ -10,8 +10,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class WebsiteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebsiteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteApplication.class, args);
+    }
 
 }
